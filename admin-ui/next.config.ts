7abaeb682f1c0262758ce9b08eb69690663471f1
vijayakumar-octablue <vijayakumardpr@gmail.com>
2026-02-12 21:56:26 +0530

    Dockerfile added

--- admin-ui/next.config.ts
+++ admin-ui/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  basePath: "/web-application-ui/admin",
 };
 
 export default nextConfig;
